eeaff8a3faf7f50ea4686ef63e0e3f3003c58385
voidaditya <dypatil.aditya00@gmail.com>
2026-08-05 04:06:36 -0400

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>   // provides strlen() prototype
+
+#define PRAISE "What a super marvelous name!"
+
+int main(void)
+{
+    char name[40];
+
+    printf("What's your name?\n");
+    scanf("%39s", name);
+
+    printf("Hello, %s. %s\n", name, PRAISE);
+
+    printf("Your name of %zu letters occupies %zu memory cells.\n",
+           strlen(name), sizeof name);
+
+    printf("The phrase of praise has %zu letters ",
+           strlen(PRAISE));
+
+    printf("and occupies %zu memory cells.\n",
+           sizeof PRAISE);
+
+    return 0;
+}
